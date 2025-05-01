e024db85090ffb8e8cd3913dae9154c0d69dfc5d
foureverhh <foureverhh@gmail.com>
2025-05-01 17:22:30 +0200

    use functional programming to replace validator

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 https://www.bilibili.com/video/BV1ds4y1E7uW?spm_id_from=333.788.videopod.sections&vd_source=d729bb6d8054d220b2d26bd4d7ac9947
 
 https://skilledcoder.medium.com/5-powerful-enum-based-patterns-in-java-279b70726ba0
+
+
+https://rameshfadatare.medium.com/stop-writing-utility-classes-the-old-way-use-functional-interfaces-instead-4b1e0d5fefc1
--- src/main/java/org/example/eum_example/enumBasedDesign/enum_validator/Validator.java
+++ src/main/java/org/example/eum_example/enumBasedDesign/enum_validator/Validator.java
@@ -1,27 +1,36 @@
 package org.example.eum_example.enumBasedDesign.enum_validator;
 
+import java.util.function.Predicate;
+
 public enum Validator {
     IS_NUMBERIC {
         @Override
         boolean validate(String input) {
-            return input.matches("\\d+");
+            //return input.matches("\\d+");
+            return numberValidator.test(input);
         }
     },
     IS_EMAIL {
         @Override
         boolean validate(String input) {
-            return input.matches("^\\w+@\\w+\\.\\w+$");
+            // return input.matches("^\\w+@\\w+\\.\\w+$");
+            return emailValidator.test(input);
         }
     },
     IS_NOT_EMPTY {
         @Override
         boolean validate(String input) {
-            return input != null && !input.trim().isEmpty();
+            // return input != null && !input.trim().isEmpty();
+            return notEmptyValidator.test(input);
         }
     };
 
 
     abstract boolean validate(String input);
+
+    Predicate<String> emailValidator =  email -> email.matches("^\\w+@\\w+\\.\\w+$");
+    Predicate<String> numberValidator = number -> number.matches("^\\d+$");
+    Predicate<String> notEmptyValidator = input -> input != null && !input.trim().isEmpty();
 
     public static void main(String[] args) {
         boolean isEmail = Validator.IS_EMAIL.validate("foureverhh@gamil.com");
